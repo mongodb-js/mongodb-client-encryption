de537521cf5d92df7f8950f9202e2a6d81daac61
bailey <bailey.pearson@mongodb.com>
2025-09-29 08:50:39 -0600

    fix lint

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,7 @@ type MongoCryptConstructorOptions = {
 };
 
 export interface MongoCryptConstructor {
-  new(options: MongoCryptConstructorOptions): MongoCrypt;
+  new (options: MongoCryptConstructorOptions): MongoCrypt;
   libmongocryptVersion: string;
 }
 
@@ -135,6 +135,6 @@ export const MongoCrypt: MongoCryptConstructor = class MongoCrypt extends mc.Mon
 
 /** exported for testing only. */
 interface MongoCryptContextCtor {
-  new(): MongoCryptContext;
+  new (): MongoCryptContext;
 }
 export const MongoCryptContextCtor: MongoCryptContextCtor = mc.MongoCryptContextCtor;
